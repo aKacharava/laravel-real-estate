d59b2a506359a3d7a0169f4f7f4e9e72174f1280
Alexander Kacharava <alexander@cube.nl>
2024-07-17 14:37:13 +0200

    Init

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+const counter = ref(0);
+
+setInterval(() => counter.value++, 1000)
+</script>
+
+<template>
+    <Link href="/">Home</Link>&nbsp;
+    <Link href="/hello">Hello</Link>
+
+    <div> counter {{ counter }} </div>
+
+    <slot></slot>
+</template>
+
+<style scoped>
+
+</style>
