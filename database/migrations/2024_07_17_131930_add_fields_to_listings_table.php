5045b88f578befaf33b071f3ef3296b723983e81
Alexander Kacharava <alexander@cube.nl>
2024-07-17 15:26:56 +0200

    Add migrations and listing model

---
+++ database/migrations/2024_07_17_131930_add_fields_to_listings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('baths');
+            $table->unsignedTinyInteger('area');
+            $table->tinyText('city')->nullable();
+            $table->tinyText('zip_code')->nullable();
+            $table->tinyText('street_name')->nullable();
+            $table->tinyText('street_number')->nullable();
+            $table->tinyInteger('price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropColumns('listings', [
+            'beds',
+            'baths',
+            'area',
+            'city',
+            'zip_code',
+            'street_name',
+            'street_number',
+            'price'
+        ]);
+    }
+};
