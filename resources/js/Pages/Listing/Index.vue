67d3f0e29ca99405d89669c8edd5b6885dfc29f5
Alexander Kacharava <alexander@cube.nl>
2024-07-22 15:27:36 +0200

    Add ui component to set global ui styling

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
-import ListingAddress from "../../Components/ListingAddress.vue";
+import ListingAddress from "@/Components/ListingAddress.vue";
 import { route } from "ziggy";
+import Box from "@/Components/UI/Box.vue";
 
 defineProps({
     listings: Array
@@ -9,18 +10,20 @@ defineProps({
 </script>
 
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <div>
-            <Link :href="route('listing.show', { listing: listing.id })">
-                <ListingAddress :listing="listing" />
-            </Link>
-        </div>
-        <div>
-            <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
-        </div>
-        <div>
-            <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button">Delete</Link>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show', { listing: listing.id })">
+                    <ListingAddress :listing="listing" />
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
+            </div>
+            <div>
+                <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button">Delete</Link>
+            </div>
+        </Box>
     </div>
 </template>
 
