40bd4e6047d193f90e9cc629b19a13094a047d22
Alexander Kacharava <alexander@cube.nl>
2024-07-24 15:14:55 +0200

    Add auth controller and routes for auth flow

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+Route::get('login/create', [\App\Http\Controllers\AuthController::class, 'create'])
+    ->name('login');
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'store'])
+    ->name('login.store');
+Route::delete('logout', [\App\Http\Controllers\AuthController::class, 'destroy'])
+    ->name('logout');
