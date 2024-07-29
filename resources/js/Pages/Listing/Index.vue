6a7b6feed2431686f7029af2c7344e5f602bada7
Alexander Kacharava <alexander@cube.nl>
2024-07-29 10:23:23 +0200

    WIP: Add filter component with styling

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Listing from "./Index/Components/Listing.vue";
-import Pagination from "../../Components/UI/Pagination.vue";
+import Pagination from "@/Components/UI/Pagination.vue";
+import Filters from "./Index/Components/Filters.vue";
 
 defineProps({
     listings: Object
@@ -8,6 +9,8 @@ defineProps({
 </script>
 
 <template>
+    <Filters />
+
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
     </div>
---
+++ resources/js/Pages/Listing/Index/Components/Filters.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+    <form>
+        <div class="mb-8 mt-4 flex flex-wrap gap-2">
+            <div class="flex flex-nowrap items-center">
+                <input
+                    type="text"
+                    placeholder="Price from"
+                    class="filter-input-left"
+                />
+                <input
+                    type="text"
+                    placeholder="Price to"
+                    class="filter-input-right"
+                />
+            </div>
+            <div class="flex flex-nowrap items-center">
+                <select class="filter-input-left">
+                    <option :value="null">Beds</option>
+                    <option v-for="n in 5" :key="n" :value="n">
+                        {{n}}
+                    </option>
+                    <option>6+</option>
+                </select>
+                <select class="filter-input-right">
+                    <option :value="null">Baths</option>
+                    <option v-for="n in 5" :key="n" :value="n">
+                        {{n}}
+                    </option>
+                    <option>6+</option>
+                </select>
+            </div>
+            <div class="flex flex-nowrap items-center">
+                <input type="text" placeholder="Area from" class="filter-input-left" />
+                <input type="text" placeholder="Area to" class="filter-input-right" />
+            </div>
+
+            <button type="submit">Filter</button>
+            <button type="reset">Clear</button>
+        </div>
+    </form>
+</template>
