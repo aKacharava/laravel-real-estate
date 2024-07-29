6ec6a12c2b508a556fe2275562f90661480e45ca
Alexander Kacharava <alexander@cube.nl>
2024-07-29 09:51:23 +0200

    Add pagination

---
+++ resources/js/Components/UI/Pagination.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+    links: Array
+})
+</script>
+
+<template>
+    <Link
+        v-for="(link, index) in links"
+        :key="index"
+        class="py-2 px-4 rounded-md"
+        :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}"
+        :href="link.url"
+        v-html="link.label"
+    />
+</template>
