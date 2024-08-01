494c0c3ffc57470abe13666c1c350a5464bde177
Alexander Kacharava <alexander@cube.nl>
2024-08-01 11:49:05 +0200

    Fix error for null url

--- resources/js/Components/UI/Pagination.vue
+++ resources/js/Components/UI/Pagination.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
 
-defineProps({
+const props = defineProps({
     links: Array
 })
 </script>
@@ -9,10 +9,10 @@ defineProps({
 <template>
     <Link
         v-for="(link, index) in links"
+        :href="link.url ?? ''"
         :key="index"
         class="py-2 px-4 rounded-md"
         :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}"
-        :href="link.url"
         v-html="link.label"
     />
 </template>
