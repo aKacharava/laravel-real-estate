7cd457351fa145b7154c756b3717a951f7b3b8a3
Alexander Kacharava <alexander@cube.nl>
2024-08-02 14:42:53 +0200

    Add templating to show page

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,7 +1,47 @@
 <script setup>
+import ListingSpace from "@/Components/UI/ListingSpace.vue";
+import ListingAddress from "@/Components/ListingAddress.vue";
+import Price from '@/Components/Price.vue';
+import Box from '@/Components/UI/Box.vue';
+import { Link } from '@inertiajs/vue3';
+import { computed } from 'vue';
+import { route } from "ziggy";
 
+const props = defineProps({
+    listing: Object
+});
+
+const hasOffers = computed(
+    () => props.listing.offers.length,
+);
 </script>
 
 <template>
-    Hi!
+    <div class="mb-4">
+        <Link
+            :href="route('realtor.listing.Index')"
+        >
+            ← Go back to Listings
+        </Link>
+    </div>
+
+    <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+            <div class="w-full text-center font-medium text-gray-500">
+                No offers
+            </div>
+        </Box>
+
+        <div v-else class="md:col-span-7 items-center">
+            This is displayed when there are offers!
+        </div>
+
+        <Box class="md:col-span-5">
+            <template #header>Basic Info</template>
+            <Price :price="listing.price" class="text-2xl font-bold" />
+
+            <ListingSpace :listing="listing" class="text-lg" />
+            <ListingAddress :listing="listing" class="text-gray-500" />
+        </Box>
+    </section>
 </template>
