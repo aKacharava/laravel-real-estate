058a84e249fefe3852bd9d0b6991d44a5f312000
Alexander Kacharava <alexander@cube.nl>
2024-08-02 16:11:49 +0200

    Simplify code for filtering accepted bids on listings

--- app/Http/Controllers/RealtorListingAcceptOfferController.php
+++ app/Http/Controllers/RealtorListingAcceptOfferController.php
@@ -11,6 +11,9 @@ class RealtorListingAcceptOfferController extends Controller
     {
         $offer->update(['accepted_at' => now()]);
 
+        $offer->listing->sold_at = now();
+        $offer->listing->save();
+
         // Reject other offers
         $offer->listing->offers()->except($offer)
             ->update(['declined_at' => now()]);
